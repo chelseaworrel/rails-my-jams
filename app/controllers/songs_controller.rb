3d2face79ce06b36d99f0ee04824e174b3711def
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-03 11:03:34 -0600

    edit song controller, and view

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,14 +10,23 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
     if @song.save
-      #do something
+      redirect_to @song #this will redirect you to the show page, could also go to a index page with 'songs_path'
     else
-      #do something
+      flash[:errors] = "Please try again" #a message that lasts for one request cycle
+      render(:new)
     end
   end
 
+  def show
+    @song = Song.find(params[:id]) #params[:id] comes from the url
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
   private
-  
+
   def song_params #use this when you get a ActiveModel::ForbiddenAttributesError
     params.require(:song).permit(:title, :artist)
   end
@@ -26,3 +35,9 @@ end
 
 #you don't need to say erb: idex, rails knows where to go
 # Song.create(params[:song]) have Morgan explain this to you....
+
+
+# C -create method
+# R -index method
+# U -
+# D
