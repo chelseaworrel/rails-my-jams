3c67b877147a81224a6ce9ce05895a265abe8dc7
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-03 10:43:13 -0600

    index all songs, form to submit new song

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,7 +2,27 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      #do something
+    else
+      #do something
+    end
+  end
+
+  private
+  
+  def song_params #use this when you get a ActiveModel::ForbiddenAttributesError
+    params.require(:song).permit(:title, :artist)
+  end
 end
 
 
-#you don't need to say erb: idex, rails knows where to go 
+#you don't need to say erb: idex, rails knows where to go
+# Song.create(params[:song]) have Morgan explain this to you....
